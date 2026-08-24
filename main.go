a537ccbdfdee12f6542af6072d5dbedc248605c6
Jufri Saja <mjhsaja@duck.com>
2026-08-24 09:07:35 +0800

    Menambahkan router dan function snippetView dan snippetCreate

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a spesific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 
